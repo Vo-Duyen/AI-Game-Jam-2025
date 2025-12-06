f83b717c13d74d28c9e979e57469fc9ba371e074
NguyenMinh205 <nqm1312@gmail.com>
2025-12-06 08:45:28 +0700

    add code

---
+++ Assets/Scripts/Core/ItemAlpha.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+
+namespace LongNC
+{
+    public class ItemAlpha : SerializedMonoBehaviour
+    {
+        [OdinSerialize] private List<Material> _materials = new List<Material>();
+        [OdinSerialize, OnValueChanged("LateUpdate")] private float _alpha = 1f;
+
+        private List<float> _alphaList = new List<float>();
+        private Color _color;
+        private float a = -1;
+
+        [Button]
+        public void Setup(bool isClearArr = true)
+        {
+            if (isClearArr)
+            {
+                _materials.Clear();
+                _alphaList.Clear();
+            }
+            
+            Queue<Transform> queue = new Queue<Transform>();
+            queue.Enqueue(transform);
+            while (queue.Count > 0)
+            {
+                Transform target = queue.Dequeue();
+
+                for (var i = 0; i < target.childCount; ++i)
+                {
+                    queue.Enqueue(target.GetChild(i));
+                }
+
+                if (target.TryGetComponent<MeshRenderer>(out var meshRenderer))
+                {
+                    _materials.Add(meshRenderer.sharedMaterial);
+                    _alphaList.Add(meshRenderer.sharedMaterial.color.a);
+                }
+            }
+        }
+
+        [Button]
+        public void SetAlpha(float alpha)
+        {
+            _alpha = alpha;
+            LateUpdate();
+        }
+
+        [Button]
+        public void DoAlpha(float alpha, float time, float delay = 0f)
+        {
+            if (_materials.Count == 0 || _alphaList.Count != _materials.Count)
+            {
+                Debug.LogWarning("Call Setup() first!");
+                return;
+            }
+            
+            for (var i = 0; i < _materials.Count; ++i)
+            {
+                _color = _materials[i].color;
+                _color.a = _alphaList[i] * alpha;
+                _materials[i].DOColor(_color, time).SetDelay(delay);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (a != _alpha)
+            {
+                a = _alpha;
+                
+                if (_materials.Count == 0 || _alphaList.Count != _materials.Count)
+                {
+                    return;
+                }
+                
+                for (var i = 0; i < _materials.Count; ++i)
+                {
+                    _color = _materials[i].color;
+                    _color.a = _alphaList[i] * a;
+                    _materials[i].color = _color;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Core/LevelData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace LongNC
+{
+    public enum ColorId
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Purple,
+        Pink,
+        Orange,
+    }
+    
+    [System.Serializable]
+    public class BlockType
+    {
+        public int colorId;
+        [ShowInInspector, ReadOnly] public string nameColor => Enum.GetName(typeof(ColorId), colorId);
+    }
+    
+    [System.Serializable]
+    public class PileStructure
+    {
+        public Vector3 defaultPosition;
+        public List<BlockType> blocks;
+    }
+    
+    [CreateAssetMenu(fileName = "DataLevel_0", menuName = "Data/Level Data")]
+    public class LevelData : ScriptableObject
+    {
+        public int level;
+        public List<int> collectionSlots = new List<int>(Enumerable.Repeat(-1, 5));
+        public List<PileStructure> pileStructures =  new List<PileStructure>();
+    }
+}
