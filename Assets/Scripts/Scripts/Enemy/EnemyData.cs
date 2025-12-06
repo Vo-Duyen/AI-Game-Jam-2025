1d3c4d9908c8b1e66fc81a9a8418d68e8baa6f60
Nguyễn Công Long <long1862004@gmail.com>
2025-12-06 10:21:50 +0700

    Update

---
+++ Assets/Scripts/Scripts/Enemy/EnemyData.cs
@@ -0,0 +1,11 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/New Enemy Data", fileName = "EnemyData")]
+public class EnemyData : SerializedScriptableObject
+{
+    public EnemyController.EnemyType typeEnemy;
+    public float maxHealth;
+    public float moveSpeed;
+    public float jumpForce;
+}
