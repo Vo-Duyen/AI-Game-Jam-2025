074ae6359a483e156075b8b48044cc1b8387ef31
NguyenMinh205 <nqm1312@gmail.com>
2025-12-07 10:01:50 +0700

    Add player animation, fix logic about player

--- Assets/Scripts/Scripts/Player/TimeControlled.cs
+++ Assets/Scripts/Scripts/Player/TimeControlled.cs
@@ -4,6 +4,14 @@ public class TimeControlled : MonoBehaviour
 {
     private Vector2 _velocity;
 
+    public SpriteRenderer MySpriteRenderer { get; private set; }
+
+    protected virtual void Awake()
+    {
+        MySpriteRenderer = GetComponent<SpriteRenderer>();
+        if (MySpriteRenderer == null) MySpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
     public Vector2 Velocity
     {
         get { return _velocity; }
