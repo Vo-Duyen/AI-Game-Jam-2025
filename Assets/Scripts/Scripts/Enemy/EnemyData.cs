c35c23f7dfaf95cadf86677b9f616396c0bdb38f
Nguyễn Công Long <long1862004@gmail.com>
2025-12-07 09:56:33 +0700

    Update enemy attack, getHit, die

--- Assets/Scripts/Scripts/Enemy/EnemyData.cs
+++ Assets/Scripts/Scripts/Enemy/EnemyData.cs
@@ -6,6 +6,7 @@ public class EnemyData : SerializedScriptableObject
 {
     public EnemyController.EnemyType typeEnemy;
     public float maxHealth;
+    public float damage;
     public float distanceCheckGround;
     public float moveSpeed;
     public float runSpeeed;
@@ -13,4 +14,5 @@ public class EnemyData : SerializedScriptableObject
     public float patrolRange;
     public float attackFollowRange;
     public float attackRange;
+    public float getHitRange;
 }
